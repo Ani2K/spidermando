dd8f3a313a0a68ddbefe419528d7f76b29c04352
casperwu <casperwu@10.19.81.44>
2017-05-12 11:05:02 -0700

    Started Working on Enemy

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,4 +1,18 @@
+import engine.*;
+import javafx.scene.image.Image;
 
-public class Enemy {
+public class Enemy extends Actor{
+	Image FLAG = new Image("/bomb_flagged.gif");;
+	
+	public Enemy(int type){
+		
+	}
+	
+	public void act(long now) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void shoot(){}
+	public void die(){}
 
 }
