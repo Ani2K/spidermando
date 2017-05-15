569114be532d6a9eaa386701a13a2ecbec7aea7e
casperwu <casperwu@10.19.87.96>
2017-05-15 11:02:17 -0700

    Gunner and Projectile updated

--- src/Gunner.java
+++ src/Gunner.java
@@ -3,24 +3,27 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class Gunner extends Actor{
-	Image FLAG = new Image("/bomb_flagged.gif");;
+	Image FLAG = new Image("/bomb_flagged.gif");
+	Image DEATH_SPRITE = new Image("/bomb_flagged.gif");
 	ImageView sprite;
+	
 	
 	public Gunner(int type){
 		sprite = new ImageView(FLAG);
 	}
 	
 	public void act(long now) {
-		// TODO Auto-generated method stub
 		
 	}
 	public void shoot(){
-		Projectile eBullet = new Projectile();
-		this.getWorld().add(eBullet);
+		Projectile enBullet = new Projectile();
+		this.getWorld().add(enBullet);
 	}
 	
 	public void die(){
-		
+		if(this.getIntersectingObjects(Projectile.class).size()>0){
+			sprite =  new ImageView(DEATH_SPRITE);
+		}
 	}
 
 }
