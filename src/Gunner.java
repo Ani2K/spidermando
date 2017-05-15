bcde16a5b89cd4cc5e2caa8f40b2fe74fe670e05
casperwu <casperwu@10.19.87.96>
2017-05-15 11:26:52 -0700

    Updated Gunner Class(dies on friendly fire)
    bomb_flagged image is only a placeholder for all images

--- src/Gunner.java
+++ src/Gunner.java
@@ -6,14 +6,27 @@ public class Gunner extends Actor{
 	Image FLAG = new Image("/bomb_flagged.gif");
 	Image DEATH_SPRITE = new Image("/bomb_flagged.gif");
 	ImageView sprite;
-	
+	double GUNNER_RANGE = 10;
 	
 	public Gunner(int type){
 		sprite = new ImageView(FLAG);
 	}
 	
 	public void act(long now) {
+		if(this.getIntersectingObjects(Projectile.class).size()!=0){
+			die();
+		}else{
 		
+			if(Math.abs(this.getX() - ((Actor) this.getWorld().getObjects(Hero.class)).getX())>GUNNER_RANGE){
+				if(this.getX() - ((Actor) this.getWorld().getObjects(Hero.class)).getX()>0){
+					this.move(5, 0);
+				}else{
+					this.move(-5, 0);
+				}
+			}else{
+				shoot();
+			}
+		}
 	}
 	public void shoot(){
 		Projectile enBullet = new Projectile();
