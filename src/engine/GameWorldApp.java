f84906280a5efefbbeb91f1ae63d92e3a6572d0d
anirudh <anirudh@10.0.0.78>
2017-05-28 12:40:25 -0700

    Menu which can transition to level 1

--- src/engine/GameWorldApp.java
+++ src/engine/GameWorldApp.java
@@ -28,6 +28,7 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
@@ -46,17 +47,17 @@ public class GameWorldApp extends Application {
 	final int SCREEN_WIDTH = BLOCK_SIZE * 10;
 	final int SCREEN_HEIGHT = BLOCK_SIZE * 10;
 	private int totalOffset = 0;
-	
+
 	private ArrayList<Block> steppingBlocks = new ArrayList<Block>();
 	private Point2D playerVelocity = new Point2D(0, 0);
 	boolean canJump = true;
 	public static boolean isAlive = true;
-	
+
 	private long last = 0;
 	static Stage theStage;
-	
+
 	Button restart;
-	
+
 	//World
 	private GameWorld world = new GameWorld();
 	public static StackPane root;
@@ -66,63 +67,83 @@ public class GameWorldApp extends Application {
 	Boss bossTest = new Boss();
 	Label healthText;
 	Label ammoText;
-	
+
 	Media gunSound = new Media(new File(new File("images/pistolsound.mp3").getAbsolutePath()).toURI().toString());
 	MediaPlayer gunPlayer = new MediaPlayer(gunSound);
 	Media spiderManSong;
 	MediaPlayer spiderPlayer;
-	
+	boolean levelStart = false;
+
 	Scene startScene;
-	
+
 	public static void main(String[] args) {
 		launch();
 	}
-	
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		//Build Level
-				theStage = primaryStage;
-				Level1 l = new Level1();
 
-				for(int i = 0; i < l.L1.length; i++){
-					String curRow = l.L1[i];
-					for(int j = 0; j < curRow.length();j++){
-						if(curRow.charAt(j)=='1'){
-							Block block = new Block(BLOCK_SIZE);
-							block.setX(j*BLOCK_SIZE);
-							block.setY(i*BLOCK_SIZE);
-							world.add(block);
-							steppingBlocks.add(block);
-						}
-						if(curRow.charAt(j)=='2'){
-							HealthPack h = new HealthPack(BLOCK_SIZE);
-							
-							h.setX(j*BLOCK_SIZE);
-							h.setY(i*BLOCK_SIZE);
-							world.add(h);
-						}
-						if(curRow.charAt(j)=='3'){
-							Munition m = new Munition(BLOCK_SIZE);
-							m.setX(j*BLOCK_SIZE);
-							m.setY(i*BLOCK_SIZE);
-							world.add(m);
-						}
-						if(curRow.charAt(j)=='4'){
-							GunnerSpawn g = new GunnerSpawn();
-							g.setX(j*BLOCK_SIZE);
-							g.setY(i*BLOCK_SIZE);
-							world.add(g);
-						}
-						
-						if(curRow.charAt(j)=='5'){
-							EndPoint g = new EndPoint(BLOCK_SIZE);
-							g.setX(j*BLOCK_SIZE);
-							g.setY(i*BLOCK_SIZE);
-							world.add(g);
-						}
-					}
+
+		Image menuImage = new Image("file:images/menuBackGround.jpg");
+		ImageView menuView = new ImageView(menuImage);
+		Pane menuPane = new Pane();
+		menuPane.setMaxWidth(menuImage.getWidth() * 0.95);
+		menuPane.setMaxHeight(menuImage.getHeight() * 0.95);
+		menuPane.getChildren().add(menuView);
+		ImageView play = new ImageView(new Image("file:images/play.png"));
+		ImageView howTo = new ImageView(new Image("file:images/howToPlay.png"));
+		VBox menuButtons = new VBox();
+		menuButtons.getChildren().addAll(play, howTo);
+		menuPane.getChildren().add(menuButtons);
+		menuButtons.setAlignment(Pos.CENTER);
+		menuButtons.setTranslateX(menuPane.getMaxWidth() / 3.25);
+		menuButtons.setTranslateY(menuPane.getMaxHeight() / 2.5);
+		Scene menuScene = new Scene(menuPane, menuImage.getWidth() * 0.95, menuImage.getHeight() * 0.95);
+		primaryStage.setScene(menuScene);
+
+		//Build Level
+		theStage = primaryStage;
+		Level1 l = new Level1();
+
+		for(int i = 0; i < l.L1.length; i++){
+			String curRow = l.L1[i];
+			for(int j = 0; j < curRow.length();j++){
+				if(curRow.charAt(j)=='1'){
+					Block block = new Block(BLOCK_SIZE);
+					block.setX(j*BLOCK_SIZE);
+					block.setY(i*BLOCK_SIZE);
+					world.add(block);
+					steppingBlocks.add(block);
 				}
-				
+				if(curRow.charAt(j)=='2'){
+					HealthPack h = new HealthPack(BLOCK_SIZE);
+
+					h.setX(j*BLOCK_SIZE);
+					h.setY(i*BLOCK_SIZE);
+					world.add(h);
+				}
+				if(curRow.charAt(j)=='3'){
+					Munition m = new Munition(BLOCK_SIZE);
+					m.setX(j*BLOCK_SIZE);
+					m.setY(i*BLOCK_SIZE);
+					world.add(m);
+				}
+				if(curRow.charAt(j)=='4'){
+					GunnerSpawn g = new GunnerSpawn();
+					g.setX(j*BLOCK_SIZE);
+					g.setY(i*BLOCK_SIZE);
+					world.add(g);
+				}
+
+				if(curRow.charAt(j)=='5'){
+					EndPoint g = new EndPoint(BLOCK_SIZE);
+					g.setX(j*BLOCK_SIZE);
+					g.setY(i*BLOCK_SIZE);
+					world.add(g);
+				}
+			}
+		}
+
 		//Background
 		Image background = new Image("file:images/background.png");
 		ImageView view = new ImageView(background);
@@ -131,39 +152,39 @@ public class GameWorldApp extends Application {
 		//World
 		root = new StackPane();
 		BorderPane pane = new BorderPane();
-		
+
 		world.setPrefWidth(SCREEN_WIDTH);
 		world.setPrefHeight(SCREEN_HEIGHT);
-		
+
 		Text title = new Text();
 		title.setText("Spidermmando");
 		title.setFont(Font.font("Verdana", FontWeight.BOLD, 20));
 		title.setFill(Color.WHITE);
 		title.setStroke(Color.web("#0d61e8"));
-		
+
 		healthText = new Label("Health: " + heroe.getHealth());
 		healthText.setFont(Font.font("Verdana", FontWeight.BOLD, 20));
 		healthText.setTextFill(Color.WHITE);
 		//healthText.setStroke(Color.web("#0d61e8"));
-		
+
 		ammoText = new Label("Ammo: " + heroe.getAmmo());
 		ammoText.setFont(Font.font("Verdana", FontWeight.BOLD, 20));
 		ammoText.setTextFill(Color.WHITE);
 		//ammoText.setStroke(Color.web("#0d61e8"));
-		
+
 		VBox infoBox = new VBox();
 		infoBox.getChildren().addAll(healthText, ammoText);
 		infoBox.setAlignment(Pos.TOP_LEFT);
-		
+
 		pane.setTop(title);
+
 		
-		root.getChildren().addAll(view, world, infoBox);
-		
+
 		world.add(heroe);
 		heroe.setBlocks(steppingBlocks);
 		heroe.setX(50);
 		heroe.setY(100); 
-		
+
 		heroe.translateXProperty().addListener((obs,old,newValue) ->{
 			int offset = newValue.intValue();
 			totalOffset += offset;
@@ -174,62 +195,62 @@ public class GameWorldApp extends Application {
 			}
 		});
 		//Testing Gunner Class
-		
+
 		world.add(gunnerTest);
 		gunnerTest.setX(10);
 		gunnerTest.setY(325);
-		
+
 		world.add(bossTest);
 		bossTest.setX(50);
 		bossTest.setY(50);
 
-		world.start();
 
-		
+
+
 		Scene scene = new Scene(root, SCREEN_WIDTH + 600, SCREEN_HEIGHT);
 		scene.addEventHandler(KeyEvent.KEY_PRESSED, new MyKeyboardHandler());
-		primaryStage.setScene(scene);
+
 		primaryStage.setResizable(false);
 		startScene = scene;
-		
-//		Image deathImage = new Image("file:images/gameOver.jpg");
-//		ImageView newRoot = new ImageView(deathImage);
-//		newRoot.setFitWidth(l.L1[0].length() * BLOCK_SIZE);
-//		Scene deathScene = new Scene(root, SCREEN_WIDTH + 600, SCREEN_HEIGHT);
-		
+
+		//		Image deathImage = new Image("file:images/gameOver.jpg");
+		//		ImageView newRoot = new ImageView(deathImage);
+		//		newRoot.setFitWidth(l.L1[0].length() * BLOCK_SIZE);
+		//		Scene deathScene = new Scene(root, SCREEN_WIDTH + 600, SCREEN_HEIGHT);
+
 		scene.setOnKeyPressed(new EventHandler<KeyEvent>(){
 
 			@Override
 			public void handle(KeyEvent e) {
 				if(e.getCode() == KeyCode.D){
-//					heroe.setDx(SPEED_OF_HERO);
-//					heroe.setRotationAxis(Rotate.Y_AXIS);
-//			    	//heroe.setRotate(360);
-//			    	heroe.setDirection(true);
-//			    	//System.out.println(heroe.getTranslateX());
-					
+					//					heroe.setDx(SPEED_OF_HERO);
+					//					heroe.setRotationAxis(Rotate.Y_AXIS);
+					//			    	//heroe.setRotate(360);
+					//			    	heroe.setDirection(true);
+					//			    	//System.out.println(heroe.getTranslateX());
+
 					heroe.setDx(30);
 					heroe.setDirection(true);
 					//moveHeroX(SPEED_OF_HERO);
 				}
 				if(e.getCode() == KeyCode.A){
-//					heroe.setDx(-1 * SPEED_OF_HERO);
-//					heroe.setRotationAxis(Rotate.Y_AXIS);
-//			    	//heroe.setRotate(180);
-//			    	heroe.setDirection(false);
-//			    	//System.out.println(heroe.getTranslateX());
+					//					heroe.setDx(-1 * SPEED_OF_HERO);
+					//					heroe.setRotationAxis(Rotate.Y_AXIS);
+					//			    	//heroe.setRotate(180);
+					//			    	heroe.setDirection(false);
+					//			    	//System.out.println(heroe.getTranslateX());
 					//moveHeroX(-1 * SPEED_OF_HERO);
 					heroe.setDx(-30);
 					heroe.setDirection(false);
 				}
 				if(e.getCode() == KeyCode.W){
-//					heroe.setDy(-50);	
+					//					heroe.setDy(-50);	
 					jumpHero();
 				}
-				
-				
+
+
 			}
-			
+
 		});
 		scene.setOnKeyReleased(new EventHandler<KeyEvent>(){
 
@@ -239,7 +260,7 @@ public class GameWorldApp extends Application {
 					heroe.setDx(0);
 				}
 			}
-			
+
 		});
 		root.setOnMouseClicked(new EventHandler<MouseEvent>(){
 
@@ -269,31 +290,31 @@ public class GameWorldApp extends Application {
 						angle = Math.PI + angle;
 						heroe.setDirection(false);
 						heroe.setRotationAxis(Rotate.Y_AXIS);
-				    	heroe.setRotate(180);
+						heroe.setRotate(180);
 					}else if(mouseX > heroX && mouseY > heroY){
 						//dx = speed * Math.cos(angle);
 						//dy = speed * Math.sin(angle);
 						angle = 2 * Math.PI - angle;
 						heroe.setDirection(true);
 						heroe.setRotationAxis(Rotate.Y_AXIS);
-				    	heroe.setRotate(360);
+						heroe.setRotate(360);
 					}else if(mouseX < heroX && mouseY < heroY){
 						//dx =  -1 * speed * Math.cos(angle);
 						//dy = -1 * speed * Math.sin(angle);
 						angle = Math.PI - angle;
 						heroe.setDirection(false);
 						heroe.setRotationAxis(Rotate.Y_AXIS);
-				    	heroe.setRotate(180);
+						heroe.setRotate(180);
 					}else{
 						//dx =  speed * Math.cos(angle);
 						//dy = -1 * speed * Math.sin(angle);
 						heroe.setDirection(true);
 						heroe.setRotationAxis(Rotate.Y_AXIS);
-				    	heroe.setRotate(360);
+						heroe.setRotate(360);
 					}
 					dx = speed * Math.cos(angle);
 					dy = -1 * speed * Math.sin(angle);
-					
+
 					angle *= (180.0 / Math.PI);
 					heroe.shoot(dx, dy, angle);
 					gunPlayer.play();
@@ -307,8 +328,24 @@ public class GameWorldApp extends Application {
 		spiderManSong = new Media(new File(path).toURI().toString());
 		spiderPlayer = new MediaPlayer(spiderManSong);
 		spiderPlayer.setVolume(0.2);
-		spiderPlayer.play();
-		
+
+
+		play.setOnMouseClicked(new EventHandler<MouseEvent>(){
+
+			@Override
+			public void handle(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				primaryStage.setScene(scene);
+				world.start();
+				spiderPlayer.play();
+				levelStart = true;
+				root.getChildren().addAll(view, world, infoBox);
+				root.setLayoutX(0);
+				infoBox.setAlignment(Pos.TOP_LEFT);
+			}
+
+		});
+
 		StringProperty fpsString = new 	SimpleStringProperty();
 		primaryStage.titleProperty().bind(fpsString);
 		AnimationTimer timer = new AnimationTimer(){
@@ -316,34 +353,34 @@ public class GameWorldApp extends Application {
 			@Override
 			public void handle(long arg0) {
 				// TODO Auto-generated method stub
-				if(gameOver == false){
+				if(gameOver == false && levelStart){
 					update();
-					
+
 					long timeElapsed = arg0 - last;
 					//fpsString.setValue(Double.toString(1_000_000_000.0/(timeElapsed)));
-					
+
 					last = arg0;
-//					if(!isAlive){
-//						primaryStage.setScene(deathScene);
-//						primaryStage.show();
-//					}
+					//					if(!isAlive){
+					//						primaryStage.setScene(deathScene);
+					//						primaryStage.show();
+					//					}
 					ammoText.setText("Ammo: " + heroe.getAmmo());
 					healthText.setText("Health: " + heroe.getHealth());
 					infoBox.setTranslateX(-1 * root.getLayoutX());
 				}
 			}
-			
+
 		};
 		timer.start();
 	}
-	
+
 	public static StackPane getRoot() {
 		return root;
 	}
 
 	private void update(){
 		if(heroe.getTranslateY() < 550){
-			
+
 			if(playerVelocity.getY() < 10){
 				playerVelocity = playerVelocity.add(0, 1);
 			}
@@ -359,7 +396,7 @@ public class GameWorldApp extends Application {
 				heroe.setX(50);
 				heroe.setY(100); 
 				spiderPlayer.play();
-				
+
 			});
 			Label dead = new Label("You have fallen to death");
 			hi.getChildren().addAll(dead,restart);
@@ -369,10 +406,10 @@ public class GameWorldApp extends Application {
 			theStage.setScene(new Scene(root,world.getWidth(),world.getHeight()));
 			spiderPlayer.stop();
 			world.stop();
-			
+
 		}
 	}
-	
+
 	private void moveHeroX(int velocity){
 		boolean movingRight = velocity > 0;
 		for(int i = 0; i < Math.abs(velocity); i++){
@@ -390,7 +427,7 @@ public class GameWorldApp extends Application {
 			heroe.setTranslateX(heroe.getTranslateX() + (movingRight ? 1 : -1));
 		}
 	}
-	
+
 	private void moveHeroY(int velocity){
 		boolean movingDown = velocity > 0;
 		for(int i = 0; i < Math.abs(velocity); i++){
@@ -410,20 +447,20 @@ public class GameWorldApp extends Application {
 			heroe.setTranslateY(heroe.getTranslateY() + (movingDown ? 1 : -1));
 		}
 	}
-	
+
 	private void jumpHero(){
 		if(canJump){
 			playerVelocity = playerVelocity.add(0, -30);
 			canJump = false;
 		}
-		
+
 	}
-	
+
 	private class MyKeyboardHandler implements EventHandler<KeyEvent> {
 
 		@Override
 		public void handle(KeyEvent e) {
-			
+
 		}
 	}
 }
